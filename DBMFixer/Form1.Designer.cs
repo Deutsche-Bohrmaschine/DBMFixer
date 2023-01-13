6012a9a067f3ec9b4f6d58406cfe9ab386ddb389
Mario Rauch <mario-68@gmx.at>
2023-01-13 17:37:59 +0100

    Projektdateien hinzufügen.

---
+++ DBMFixer/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace DBMFixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 202);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(430, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 184);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 120);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(58, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Full Fix";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 97);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(129, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Check for broken files";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 74);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(95, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Delete Configs";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 51);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(107, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Reinstall Battleye";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Delete all Logs";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(199, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(243, 146);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Logs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Steam Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Alive";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "DayZ Installed:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(238, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "NO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(269, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Connected as:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(342, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Copy Steam Id";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 258);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DBM Fixer | v 1.0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DBMFixer/Form1.cs
@@ -0,0 +1,191 @@
+﻿using Steamworks;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace DBMFixer
+{
+    public partial class Form1 : Form
+    {
+        private string dayZLogPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DayZ");
+        private string dayZInstallPath { get; set; }
+        private const long OneKb = 1024;
+        private const long OneMb = OneKb * 1024;
+        private const long OneGb = OneMb * 1024;
+        private const long OneTb = OneGb * 1024;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task.Factory.StartNew(() => { StartAction(); });
+            }
+            catch
+            {
+                WriteLog("A Error has been occured!");
+            }
+        }
+
+        private void StartAction()
+        {
+            if (checkBox1.Checked)
+            {
+                DeleteAllLogs();
+            }
+
+            if (checkBox2.Checked)
+            {
+                ReinstallBattleye();
+            }
+        }
+
+        private Task ReinstallBattleye()
+        {
+            var battleyeDir = Path.Combine(dayZInstallPath, "BattlEye");
+            if (Directory.Exists(battleyeDir))
+            {
+                WriteLog($"found battleye on {battleyeDir}");
+                var consolePath = Path.Combine(battleyeDir, "Uninstall_BattlEye.bat");
+                var installPathConsole = Path.Combine(battleyeDir, "Install_BattlEye.bat");
+                WriteLog($"start uninstall BE!");
+                Process.Start(consolePath);
+                WriteLog($"BE -> Uninstalled!");
+                WriteLog($"Start Install ....");
+                Process.Start(installPathConsole);
+                WriteLog("BE Reinstalled!");
+            }
+            return Task.CompletedTask;
+        }
+
+        private void WriteLog(string log)
+        {
+
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(WriteLog), new object[] { log });
+                return;
+            }
+
+
+            if (richTextBox1.Text == "")
+                richTextBox1.Text = log;
+            else
+                richTextBox1.Text += Environment.NewLine + log;
+        }
+
+        private void InvokeProgressbar(int value, int max)
+        {
+
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<int, int>(InvokeProgressbar), new object[] { value, max });
+                return;
+            }
+
+
+            progressBar1.Maximum = max;
+            progressBar1.Value = value;
+        }
+
+        private string ToPrettySize(long value, int decimalPlaces = 0)
+        {
+            var asTb = Math.Round((double)value / OneTb, decimalPlaces);
+            var asGb = Math.Round((double)value / OneGb, decimalPlaces);
+            var asMb = Math.Round((double)value / OneMb, decimalPlaces);
+            var asKb = Math.Round((double)value / OneKb, decimalPlaces);
+            string chosenValue = asTb > 1 ? string.Format("{0}Tb", asTb)
+                : asGb > 1 ? string.Format("{0}Gb", asGb)
+                : asMb > 1 ? string.Format("{0}Mb", asMb)
+                : asKb > 1 ? string.Format("{0}Kb", asKb)
+                : string.Format("{0}B", Math.Round((double)value, decimalPlaces));
+            return chosenValue;
+        }
+
+        private Task DeleteAllLogs()
+        {
+            if (Directory.Exists(dayZLogPath))
+            {
+                WriteLog($"Successfully found dayz log path on: {dayZLogPath}");
+                var files = Directory.GetFiles(dayZLogPath);
+                var logCount = 0;
+                long takenDiskSize = 0;
+
+                foreach (var file in files)
+                {
+                    FileInfo info = new FileInfo(file);
+                    if(info != null && info.Exists)
+                    {
+                        if(info.Extension == ".log" || info.Extension == ".RPT" || info.Extension == ".mdmp")
+                        {
+                            logCount++;
+                            takenDiskSize += info.Length;
+                        }
+                    }
+                }
+                WriteLog($"Found: {logCount} to delete.");
+                WriteLog($"Takes: {ToPrettySize(takenDiskSize)} disk size.");
+
+                WriteLog("Starting to delete files.");
+                InvokeProgressbar(0, logCount);
+                int counter = 0;
+                foreach (var file in files)
+                {
+                    FileInfo info = new FileInfo(file);
+                    if (info != null && info.Exists)
+                    {
+                        if (info.Extension == ".log" || info.Extension == ".RPT" || info.Extension == ".mdmp")
+                        {
+                            info.Delete();
+                            counter++;
+                            InvokeProgressbar(counter, logCount);
+                        }
+                    }
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Steamworks.SteamClient.Init(221100, true);
+                label3.Text = "Alive and Running!";
+                dayZInstallPath = Steamworks.SteamApps.AppInstallDir();
+                if (Steamworks.SteamApps.IsAppInstalled(221100))
+                {
+                    label5.Text = "Yes";
+                    MessageBox.Show(dayZInstallPath);
+                }
+                else
+                {
+                    label5.Text = "No";
+                }
+
+
+                label7.Text = SteamClient.Name;
+            }
+            catch (Exception exception)
+            {
+                // Something went wrong! Steam is closed?
+                MessageBox.Show($"{exception.Message} \nDBM Fixer will exit now!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label3.Text = "Dead!";
+                label5.Text = "No";
+                label7.Text = "Unkown";
+                Environment.Exit(0);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Steamworks.SteamClient.SteamId.Value.ToString());
+        }
+    }
+}
